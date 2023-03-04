4c06d196de2cf573478d397d7af3b6046ed1bda6
Mobcn <mobingcn@gmail.com>
2023-03-04 16:00:52 +0800

    init

---
+++ import.js
@@ -0,0 +1,19 @@
+/**
+ * import setting
+ */
+(() => ({
+    // tag setting
+    importToTag: {
+        js: [],
+        css: []
+    },
+
+    // ESM setting
+    importToESM: {
+        // Vue3 SFC Loader
+        'vue3-sfc-loader': 'https://npm.elemecdn.com/vue3-sfc-loader@0.8.4/dist/vue3-sfc-loader.esm.js',
+
+        // Vue3
+        vue: 'https://cdn.staticfile.org/vue/3.2.37/vue.esm-browser.prod.min.js'
+    }
+}))();
